7e406b05bcbcc52307bf2b144d036e92eaa8222d
aeddaqqa <achrafeddaqqaq@gmail.com>
2021-12-25 18:50:52 +0100

    add useForm hook

--- client/src/components/forms/LoginFrom.js
+++ client/src/components/forms/LoginFrom.js
@@ -1,83 +1,37 @@
 import { LoginFromStyle } from './LoginForm.style';
-import { Form, Input, Button, Checkbox } from 'antd';
-import 'antd/dist/antd.css';
+import useForm from '../../Hooks/useForm';
 
 const Login = (props) => {
-    const onFinish = (values) => {
-        console.log('Success:', values);
+    const formLogin = () => {
+        console.log('Callback function when form is submitted!');
+        console.log('Form Values ', values);
     };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+    //Custom hook call
+    const { handleChange, values, errors, handleSubmit } = useForm(formLogin);
     return (
         <LoginFromStyle>
             <h1>
                 <span> Welcome back! </span>Please login to your account.
             </h1>
-            <Form
-                name="login"
-                labelCol={{
-                    span: 8,
-                }}
-                wrapperCol={{
-                    span: 16,
-                }}
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete="off"
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your username!',
-                        },
-                    ]}
-                >
-                    <Input />
-                </Form.Item>
-
-                <Form.Item
-                    label="Password"
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="E-mail"
+                    onChange={handleChange}
+                />
+                {errors.email && <h3>{errors.email}</h3>}
+                <input
+                    minLength="8"
+                    type="password"
                     name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your password!',
-                        },
-                    ]}
-                >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="remember"
-                    valuePropName="checked"
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-
-                <Form.Item
-                    wrapperCol={{
-                        offset: 8,
-                        span: 16,
-                    }}
-                >
-                    <Button type="primary" htmlType="submit">
-                        Submit
-                    </Button>
-                </Form.Item>
-            </Form>
+                    placeholder="password"
+                    onChange={handleChange}
+                />
+                {errors.password && <h3>{errors.password}</h3>}
+                <input type="submit" value="Submit" className="submit" />
+            </form>
         </LoginFromStyle>
     );
 };
