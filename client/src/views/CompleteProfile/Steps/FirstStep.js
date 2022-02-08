8e878ed341d03c32b5d3a1df47624e1152c04168
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-02-08 14:02:28 +0100

    start first step

--- client/src/views/CompleteProfile/Steps/FirstStep.js
+++ client/src/views/CompleteProfile/Steps/FirstStep.js
@@ -1,45 +1,45 @@
 import { StyledFirstStep } from "./FirstStep.Style";
 import TextField from "@mui/material/TextField";
-import TextareaAutosize from "@mui/material/TextareaAutosize";
-import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import { Radio } from "antd";
+import { useState } from "react";
+import { DatePicker } from "antd";
+import moment from "moment";
+import { Input } from "antd";
+import "antd/dist/antd.css";
+
+const { TextArea } = Input;
 /*
-  firstName: null,
-    lastName: null,
-    gender: null,
-    biography: null,
-    birthdate: null,
+  firstName: null, // done
+    lastName: null, // done
+    gender: null, //done
+    birthdate: null, // done
+    biography: null, //done
     isComplete: false,
 */
 const FirstStep = ({ state, dispatch }) => {
+  const [value, setValue] = useState(1);
+  const { RangePicker } = DatePicker;
+  const dateFormat = "YYYY/MM/DD";
+  const onChange = (e) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
+  };
+
   return (
     <StyledFirstStep>
-      <TextField
-        id="filled-basic"
-        margin="normal"
-        label="first name"
-        variant="filled"
+      <TextField id="standard-basic" label="First Name" variant="standard" />
+      <TextField id="standard-basic" label="Last Name" variant="standard" />
+      <Radio.Group onChange={onChange} value={value}>
+        <Radio value={1}>A</Radio>
+        <Radio value={2}>B</Radio>
+        <Radio value={3}>C</Radio>
+        <Radio value={4}>D</Radio>
+      </Radio.Group>
+      <DatePicker
+        defaultValue={moment("2015/01/01", dateFormat)}
+        format={dateFormat}
       />
-      <TextField
-        id="filled-basic"
-        label="last name"
-        margin="normal"
-        variant="filled"
-      />
-      <TextareaAutosize
-        maxRows={4}
-        aria-label="maximum height"
-        placeholder="Maximum 4 rows"
-        defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua."
-        style={{ width: 200 }}
-      />
-      {/* <DesktopDatePicker
-        label="Date desktop"
-        inputFormat="MM/dd/yyyy"
-        value={value}
-        onChange={handleChange}
-        renderInput={(params) => <TextField {...params} />}
-      /> */}
+      <TextArea showCount maxLength={150} onChange={onChange} />
     </StyledFirstStep>
   );
 };
