276b9829cfe63250d80ad7a23d76f8f09ab243a0
aeddaqqa <achrafeddaqqaq@gmail.com>
2022-02-13 20:49:12 +0100

    add map

---
+++ client/src/views/CompleteProfile/Steps/ThirdStep.Style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ThirdStepStyle = styled.div`
+    width: 100%;
+    height: 800px;
+    /* background-color: blue; */
+    .map-container {
+        height: 500px;
+        width: 500px;
+    }
+`;
--- client/src/views/CompleteProfile/Steps/ThirdStep.js
+++ client/src/views/CompleteProfile/Steps/ThirdStep.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { ThirdStepStyle } from "./ThirdStep.Style";
+
 const ThirdStep = () => {
-  return <div>third step</div>;
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log({ coords: position.coords });
+    });
+
+    return (
+        <ThirdStepStyle>
+            <MapContainer
+                className="map-container"
+                center={[51.505, -0.09]}
+                zoom={13}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </ThirdStepStyle>
+    );
 };
 
 export default ThirdStep;
