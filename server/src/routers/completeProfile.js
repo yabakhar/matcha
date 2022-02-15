854cc9fafd30e10d544f0874d53464d198de072a
yabakhar <youssefabkhar52@gmail.com>
2022-02-15 10:35:05 +0100

    insert tags and photos in complete profile

--- server/src/routers/completeProfile.js
+++ server/src/routers/completeProfile.js
@@ -2,14 +2,23 @@ const router = require('express').Router()
 const db = require('../database/database.js')
 const auth = require('../middlewares/auth')
 router.post("/completeProfile", auth,(req, res) => {
-    const {location,sexualPreferences,firstName , lastName,gender,biography,birthdate,avatar,id} = req.body;
-    db.query('UPDATE users SET first_name=?,last_name=?,gender=?,orientation=?,biography=?,lat=?,lon=?,birthdate=?,avatar=? WHERE id=?',[firstName,lastName,gender,sexualPreferences,biography,location.lat,location.lon,birthdate,avatar,id],function (err, result, fields) {
+    const {location,sexualPreferences,firstName , lastName,gender,biography,birthdate,avatar,id,listOfInterests,gallery} = req.body;
+    const tags = [];
+    const photos = [];
+    listOfInterests.forEach(element => {
+        tags.push([id,element]);
+    });
+    gallery.forEach(element => {
+        photos.push([id,element]);
+    });
+    db.query('UPDATE users SET first_name=?,last_name=?,gender=?,orientation=?,biography=?,lat=?,lon=?,birthdate=?,avatar=? WHERE id=?;INSERT INTO tags(iduser,tag) VALUES ?;INSERT INTO photos(iduser,photo) VALUES ?',
+    [firstName,lastName,gender,sexualPreferences,biography,location.lat,location.lon,birthdate,avatar,id,tags,photos],function (err, result, fields) {
         if (err) 
             return res.status(400)
             .json(
                 {
                     status: 400,
-                    message: "Database error ",
+                    message: "Database error " + err,
                 }
             )
         else
@@ -19,17 +28,16 @@ router.post("/completeProfile", auth,(req, res) => {
                 {
                     status: 200,
                     message: "saccses",
-                    result:result
+                    result: result
                 }
             )
         }
       });
 });
 module.exports = router;
-
-// UPDATE Customers
-// SET ContactName = 'Alfred Schmidt', City = 'Frankfurt'
-// WHERE CustomerID = 1;
+// 
+// 
+//;INSERT INTO photos(iduser,photo) VALUES ?
 /*
 calculate age
 */
